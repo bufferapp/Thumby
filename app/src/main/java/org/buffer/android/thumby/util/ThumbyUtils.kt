4d3279998495303bd8e4d63b30195383bc385a42
Emre Hamurcu <emrehamurcu_1995@hotmail.com>
2020-05-17 17:56:28 +0300

    Migrate to androidx Add to realPath function
    Signed-off-by: Emre Hamurcu <emrehamurcu_1995@hotmail.com>

--- app/src/main/java/org/buffer/android/thumby/util/ThumbyUtils.kt
+++ app/src/main/java/org/buffer/android/thumby/util/ThumbyUtils.kt
@@ -18,15 +18,7 @@ object ThumbyUtils {
     ): Bitmap {
         val mMMR = MediaMetadataRetriever()
         mMMR.setDataSource(context, uri)
-        var bitmap = mMMR.getFrameAtTime(frameTime)
-        /*   val mediaMetadataRetriever = MediaMetadataRetriever()
-           if (ApiLevelHelper.isAtLeast(Build.VERSION_CODES.Q)) {
-               mediaMetadataRetriever.setDataSource(getRealPathFromURI(context,uri))
-           } else {
-               mediaMetadataRetriever.setDataSource(context, uri)
-           }
-           var bitmap = mediaMetadataRetriever.getFrameAtTime(frameTime,
-               MediaMetadataRetriever.OPTION_CLOSEST_SYNC)*/
+        var bitmap = mMMR.getFrameAtTime(frameTime, MediaMetadataRetriever.OPTION_CLOSEST_SYNC)
         try {
             bitmap = Bitmap.createScaledBitmap(bitmap, width, height, false)
         } catch (e: Exception) {
